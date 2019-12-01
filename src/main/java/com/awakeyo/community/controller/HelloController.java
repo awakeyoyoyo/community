a9ce70b8c97d48b803f61566d62d0139b88e96ae
awakeyoyoyo <lqh19980716@163.com>
2019-12-01 14:32:33 +0800

    初始化项目

---
+++ src/main/java/com/awakeyo/community/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.awakeyo.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author awakeyoyoyo
+ * @className HelloController
+ * @description TODO
+ * @date 2019-11-30 21:13
+ */
+@Controller
+public class HelloController {
+    /**
+     * Method Description
+     * @author awakeyoyoyo
+     * @date 2019-11-30
+     * @params [name, model]
+     * @return java.lang.String
+     */
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
