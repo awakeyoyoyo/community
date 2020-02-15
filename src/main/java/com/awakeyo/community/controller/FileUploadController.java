ba3e4c4572259785005ca0f2d76534e0804a0de5
awakeyoyoyo <lqh19980716@163.com>
2020-02-15 23:15:49 +0800

    阿里oss上传图片2.0

--- src/main/java/com/awakeyo/community/controller/FileUploadController.java
+++ src/main/java/com/awakeyo/community/controller/FileUploadController.java
@@ -1,9 +1,21 @@
 package com.awakeyo.community.controller;
 
 import com.awakeyo.community.pojo.dto.ImageEditDto;
+import com.awakeyo.community.provider.AlibabaOssProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+import static org.springframework.util.ResourceUtils.getFile;
 
 /**
  * @author awakeyoyoyo
@@ -13,12 +25,28 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class FileUploadController {
+    @Autowired
+    private AlibabaOssProvider alibabaOssProvider;
+    @Value("${alibabaoss.communityfolder}")
+    private String folder;
+    @Value("${alibabaoss.url}")
+    private String endpoint;
     @RequestMapping("/image/upload")
     @ResponseBody
-    public ImageEditDto upload(){
+    public ImageEditDto Imageupload(@RequestParam(value = "editormd-image-file") MultipartFile file, HttpServletRequest request){
+        String fileName="";
+        try {
+            fileName=alibabaOssProvider.upload(file.getInputStream(),file.getContentType(),file.getOriginalFilename(),folder);
+            if (fileName==null){
+                fileName="gg.jpg";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         ImageEditDto imageEditDto=new ImageEditDto();
         imageEditDto.setSuccess(1);
-        imageEditDto.setUrl("/images/loading.gif");
+        imageEditDto.setUrl(endpoint+fileName);
         return imageEditDto;
     }
 }
--- src/main/java/com/awakeyo/community/provider/AlibabaOssProvider.java
+++ src/main/java/com/awakeyo/community/provider/AlibabaOssProvider.java
@@ -1,8 +1,19 @@
 package com.awakeyo.community.provider;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.internal.OSSHeaders;
+import com.aliyun.oss.model.CannedAccessControlList;
+import com.aliyun.oss.model.ObjectMetadata;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.StorageClass;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.UUID;
 
 /**
  * @author awakeyoyoyo
@@ -10,15 +21,38 @@ import org.springframework.stereotype.Service;
  * @description TODO
  * @date 2020-02-13 22:27
  */
-@Component
+@Service
 public class AlibabaOssProvider {
     @Value("${alibabaoss.accessKeyId}")
     private String accessKeyId;
-    @Value("${alibabaoss.accessKeyId}")
+    @Value("${alibabaoss.accessKeySecret}")
     private String accessKeySecret;
     @Value("${alibabaoss.bucketName}")
     private String bucketName;
     @Value("${alibabaoss.endpoint}")
     private String endpoint;
-
+    public String upload(InputStream fileStream,String mineType,String fileName,String folder){
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+        // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
+        //上传文件到OSS时需要指定包含文件后缀在内的完整路径，例如abc/efg/123.jpg。
+        String generateFileName=folder+"/";
+        String[] fileSpliter=fileName.split("\\.");
+        if (fileSpliter.length>1){
+            generateFileName= generateFileName+UUID.randomUUID().toString()+"."+fileSpliter[fileSpliter.length-1];
+        }else {
+            return null;
+        }
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        // 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, generateFileName,fileStream);
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType("image/jpg");
+        putObjectRequest.setMetadata(metadata);
+        // 上传内容到指定的存储空间（bucketName）并保存为指 定的文件名称（objectName）。
+        ossClient.putObject(putObjectRequest);
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        return "/"+generateFileName;
+    }
 }
