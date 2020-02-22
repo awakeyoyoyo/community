de8eb5710aa133bdc077588241716696093bf15d
awakeyoyoyo <lqh19980716@163.com>
2020-02-22 20:46:45 +0800

    挖新坑

---
+++ src/main/java/com/awakeyo/community/provider/OkHttpProvider.java
@@ -0,0 +1,10 @@
+package com.awakeyo.community.provider;
+
+/**
+ * @author awakeyoyoyo
+ * @className OkHttpProvider
+ * @description TODO
+ * @date 2020-02-22 20:15
+ */
+public class OkHttpProvider {
+}
